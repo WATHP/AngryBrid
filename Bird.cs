c9f9c4b151c4babcf93597e8720eef856f7cbbef
WATHP <594312342@qq.com>
2017-06-30 18:20:40 +0800

    测试

---
+++ Bird.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    public float timer;
+    public int frameNumber = 10;    //frame number one seconds
+    public int frameCount = 0;      //frame counter
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer >= 1.0f / frameNumber)
+        {
+            frameCount++;
+            timer -= 1.0f / frameNumber;
+            int frameIndex = frameCount % 3;
+            //update material's offset
+            //how to set the property of (x offset) 
+            //MainTex:着色器中主要的漫反射
+            GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(0.3333f*frameIndex, 0));
+        }
+	}
+}
